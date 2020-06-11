be0aaaacf38effc1ea486a64c84b109bb92d68d7
16702491 <sletov.s.s@sberbank.ru>
2020-06-11 10:49:08 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -203,10 +203,10 @@ function changeNewCartItemCount(id, size, count) {
     var key = CartItemKey(id, size);
     var cartItemId = `cart-item-${id}-${size}`;
 
-    if (count === 0){
+    if (count === 0) {
         cart.delete(key);
 
-        if (cart.size === 0){
+        if (cart.size === 0) {
             createCartHtml();
             activateCartPopover();
         } else {
@@ -218,7 +218,7 @@ function changeNewCartItemCount(id, size, count) {
 
         cart.set(key, newCount);
 
-        if (newCount === 0){
+        if (newCount === 0) {
             changeNewCartItemCount(id, size, newCount);
         } else {
             document.getElementById(cartItemId).getElementsByClassName("cart-count")[0].innerHTML = newCount;
@@ -239,7 +239,7 @@ function createCartHtml() {
     if (cart.size === 0) {
         initCartPopover('Корзина пуста');
     } else {
-        var contentString = "";
+        var contentString = "<div id='cart-items-list'>";
 
         var totalPrice = 0;
 
@@ -279,7 +279,8 @@ function createCartHtml() {
         }
 
         contentString +=
-            `<div id="cart-total-info">
+            `</div>
+            <div id="cart-total-info">
             Итоговая стоимость: <span id='cart-total-price' class="font-weight-bold">${totalPrice}</span>p<br>
             <button type='button' id="cart-clean" class='btn btn-link p-2' onclick="cleanCart()">
                 <svg class='bi bi-cart-dash align-middle' width='1.2em' height='1.2em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
@@ -468,9 +469,9 @@ function initCartPopover(contentString) {
                          xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                               d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-                    </svg>
-                        Корзина`;
-    })
+            </svg>
+            Корзина`;
+    });
 }
 
 function activateCartPopover() {
